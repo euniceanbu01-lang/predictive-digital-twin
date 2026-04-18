05c4f90327582c281256f914144e72fc8fd38cb5
euniceanbu01-lang <euniceanbu01@gmail.com>
2026-04-18 21:18:06 +0530

    Delete backend/scalers.py

--- backend/scalers.py
+++
@@ -1,7 +0,0 @@
-# backend/scalers.py
-from sklearn.preprocessing import RobustScaler
-
-class NamedRobustScaler(RobustScaler):
-    def __init__(self, feature_names=None, **kwargs):
-        super().__init__(**kwargs)
-        self.feature_names = feature_names
